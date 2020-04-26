9c1700097fbe6f4bc256f69b3b1fa1d0c7e9f7e9
dgkirkwood <daniel.g.kirkwood@gmail.com>
2020-04-26 21:54:22 +1000

    Change ssh public key

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "agent_count" {
 }
 
 variable "ssh_public_key" {
-    default = "~/.ssh/id_rsa.pub"
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/kjAJ0zAqMU//dhH3i7xIuedn+Ruj+YqFjGcyYLBg//4fADn2tUczZuqMw5f9DR8UR7JfypDzbbnNxJ9vn9Y7bjUyZHeZb8dc65OgBIqvmiXq5goYlOyovqXS1pdNo71GnKzgWAekgo1u/fj7Y+45jDgI3mPfGNTTM/k77xCSWiZaWHToisL3jQUV+iU9sg2w77/+4Ok+11udnSyLDVRy7tkQdLe6Gg7Nuo36uIPjEQ3BkWbv6eZwpzjT/we9UbWFgzxfQCgCfmS9vf1keLfkW9npW3m7sUKEsDb145mvDFcOkFGuotocX1LOj46Aw9JTXhr0wE/elVt308r5Mq1S97NIXkFnnsYNpfw7Yv5V0iNy8Da5w/FIIdZ8mFwMzRWDIRAy8CjYQqXB3miGeGYrfFZJh85cLSUCoGN90bujHsBt1L1ck1gtq3o5ZnY5RLy9GS70NzK/XtCM2tisn6mg4EVbNwuFo/Woi74EOahSFWVxNyk5m2k07J+5TGOjtlGL+ayZg/MQWjQZ6KolUVPG/7mRAD3A1tBt8NnEq/FHJvI1eW6JPobRWjR94pVr2oUJ5g5zHhzwjdo0cOHP5nz5Ovea2a/Yn2nUds9kRV3lZvmkZ2TeVlvyT+Nxo35uTjpMB3KtbSR8tRcgIvIjOdM0cmEXSv+TM2Gl+q/iWybY3Q== dk@hashicorp.com"
 }
 
 variable "dns_prefix" {
